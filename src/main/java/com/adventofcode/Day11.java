f974400453192dee5135e1f0c46a54a4d4f877a4
Sławek <slawek.grz@gmail.com>
2023-12-11 09:28:34 +0100

    day 11

--- src/main/java/com/adventofcode/Day11.java
+++ src/main/java/com/adventofcode/Day11.java
@@ -2,55 +2,34 @@ package com.adventofcode;
 
 import com.adventofcode.input.ArrayMap;
 import com.adventofcode.input.Input;
+import com.adventofcode.input.Pair;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Day11 {
 
     private final ArrayMap<Character> galaxiesMap;
+    private final List<Integer> rowsToExpand;
+    private final List<Integer> columnsToExpand;
+    private final List<ArrayMap.Element<Character>> galaxies;
 
     public Day11() throws IOException {
         galaxiesMap = Input.day11();
-    }
-
-    long part1() {
-        return addAllDistances(1);
-    }
-
-    long part2() {
-        return addAllDistances(999_999);
-    }
-
-    private long addAllDistances(int agingPace) {
-        List<Integer> rowsToExpand = new ArrayList<>(Collections.nCopies(galaxiesMap.getHeight(), 0));
-        List<Integer> columnsToExpand = new ArrayList<>(Collections.nCopies(galaxiesMap.getWidth(), 0));
+        rowsToExpand = new ArrayList<>(Collections.nCopies(galaxiesMap.getHeight(), 0));
+        columnsToExpand = new ArrayList<>(Collections.nCopies(galaxiesMap.getWidth(), 0));
         IntStream.range(0, galaxiesMap.getHeight())
                 .filter(this::rowHasNoGalaxy)
-                .forEach(i -> rowsToExpand.set(i, agingPace));
+                .forEach(i -> rowsToExpand.set(i, 1));
         IntStream.range(0, galaxiesMap.getWidth())
                 .filter(this::columnHasNoGalaxy)
-                .forEach(i -> columnsToExpand.set(i, agingPace));
-        List<ArrayMap.Element<Character>> galaxies = galaxiesMap.traverse()
+                .forEach(i -> columnsToExpand.set(i, 1));
+        galaxies = galaxiesMap.traverse()
                 .filter(e -> e.value().equals('#'))
                 .toList();
-        List<Long> distances = new ArrayList<>();
-        for (int i = 0; i < galaxies.size() - 1; i++) {
-            ArrayMap.Element<Character> from = galaxies.get(i);
-            for (int j = i + 1; j < galaxies.size(); j++) {
-                ArrayMap.Element<Character> to = galaxies.get(j);
-                int minX = Math.min(from.coordinates().x(), to.coordinates().x());
-                int maxX = Math.max(from.coordinates().x(), to.coordinates().x());
-                int minY = Math.min(from.coordinates().y(), to.coordinates().y());
-                int maxY = Math.max(from.coordinates().y(), to.coordinates().y());
-                long distance = maxX + maxY - minX - minY
-                        + (minX == maxX ? 0 : rowsToExpand.subList(minX + 1, maxX).stream().mapToInt(x -> x).sum())
-                        + (minY == maxY ? 0 : columnsToExpand.subList(minY + 1, maxY).stream().mapToInt(y -> y).sum());
-                distances.add(distance);
-            }
-        }
-        return distances.stream().mapToLong(d -> d).sum();
     }
 
     private boolean rowHasNoGalaxy(int x) {
@@ -63,6 +42,35 @@ public class Day11 {
         return galaxiesMap.traverseColumn(y)
                 .map(ArrayMap.Element::value)
                 .allMatch(c -> c.equals('.'));
+    }
+
+    long part1() {
+        return addAllDistances(1);
+    }
+
+    long part2() {
+        return addAllDistances(999_999);
+    }
+
+    private long addAllDistances(long agingPace) {
+        return IntStream.range(0, galaxies.size())
+                .mapToObj(i -> IntStream.range(i + 1, galaxies.size()).mapToObj(j -> new Pair<>(i, j)))
+                .flatMap(Function.identity())
+                .map(indices -> new Pair<>(galaxies.get(indices.first()), galaxies.get(indices.second())))
+                .mapToLong(pair -> {
+                    int minX = Math.min(pair.first().coordinates().x(), pair.second().coordinates().x());
+                    int maxX = Math.max(pair.first().coordinates().x(), pair.second().coordinates().x());
+                    int minY = Math.min(pair.first().coordinates().y(), pair.second().coordinates().y());
+                    int maxY = Math.max(pair.first().coordinates().y(), pair.second().coordinates().y());
+                    int emptyRowsAndColumnsInBetweenGalaxies = Stream.concat(
+                                    (minX == maxX ? Stream.empty() : rowsToExpand.subList(minX + 1, maxX).stream()),
+                                    (minY == maxY ? Stream.empty() : columnsToExpand.subList(minY + 1, maxY).stream())
+                            )
+                            .mapToInt(i -> i)
+                            .sum();
+                    return maxX + maxY - minX - minY + emptyRowsAndColumnsInBetweenGalaxies * agingPace;
+                })
+                .sum();
     }
 
 }
---
+++ src/main/java/com/adventofcode/input/Pair.java
@@ -0,0 +1,4 @@
+package com.adventofcode.input;
+
+public record Pair<T>(T first, T second) {
+}
